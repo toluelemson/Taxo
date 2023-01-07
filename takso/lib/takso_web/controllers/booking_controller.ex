a8b95c5beafef7e6eb2e41edbeedc4fcfbba73d5
Toluwani <toluwhani@gmail.com>
2023-01-07 11:27:16 +0200

    Adding users

--- takso/lib/takso_web/controllers/booking_controller.ex
+++ takso/lib/takso_web/controllers/booking_controller.ex
@@ -2,7 +2,10 @@ defmodule TaksoWeb.BookingController do
   use TaksoWeb, :controller
 
   def new(conn, _params) do
+
     render conn, "new.html"
+
   end
+
 
 end
--- takso/lib/takso_web/controllers/user_controller.ex
+++ takso/lib/takso_web/controllers/user_controller.ex
@@ -5,8 +5,25 @@ defmodule TaksoWeb.UserController do
   alias Takso.Accounts.User
 
   def index(conn, _params) do
+
     users = Repo.all(User)
     render(conn, "index.html", users: users)
+
+  end
+
+  def new(conn, _params) do
+
+    changeset = User.changeset(%User{}, %{})
+    render(conn, "new.html", changeset: changeset)
+
+  end
+
+  def create(conn, %{"user" => user_params}) do
+
+    changeset = User.changeset(%User{}, user_params)
+    Repo.insert(changeset)
+    redirect(conn, to: Routes.user_path(conn, :index))
+
   end
 
 end
