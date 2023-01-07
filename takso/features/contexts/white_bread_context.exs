c7b1239d1f9c7e76910f8465c65439a6d90309ba
Toluwani <toluwhani@gmail.com>
2023-01-07 03:52:35 +0200

    fix error in white bread

--- takso/features/contexts/white_bread_context.exs
+++ takso/features/contexts/white_bread_context.exs
@@ -1,24 +1,27 @@
 defmodule WhiteBreadContext do
   use WhiteBread.Context
-  use Hound.Helper
+  use Hound.Helpers
 
-  feature starting_state fn ->
+  feature_starting_state fn ->
     Application.ensure_all_started(:hound)
-    ${}
+    %{}
   end
 
-  scenario_starting_state fn ->
+  scenario_starting_state fn _state->
     Hound.start_session
     %{}
   end
 
-  scenario_finalize_state fn ->
-    Hound.end_session
-    %{}
+  scenario_finalize fn status, _state->
+    nil
   end
 
-
   given_ ~r/^the following taxis are on duty$/, fn state ->
+    {:ok, state}
+  end
+
+  and_ ~r/^I open STRS' web page$/, fn state ->
+    navigate_to "/bookings/new"
     {:ok, state}
   end
 end
