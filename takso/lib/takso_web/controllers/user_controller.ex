2337a2b1ce7bf769809993181c5a6cde5bf4e7ca
Toluwani <toluwhani@gmail.com>
2023-01-07 12:06:00 +0200

    Editing users

--- takso/lib/takso_web/controllers/user_controller.ex
+++ takso/lib/takso_web/controllers/user_controller.ex
@@ -5,25 +5,31 @@ defmodule TaksoWeb.UserController do
   alias Takso.Accounts.User
 
   def index(conn, _params) do
-
     users = Repo.all(User)
     render(conn, "index.html", users: users)
-
   end
 
   def new(conn, _params) do
-
     changeset = User.changeset(%User{}, %{})
     render(conn, "new.html", changeset: changeset)
-
   end
 
   def create(conn, %{"user" => user_params}) do
-
     changeset = User.changeset(%User{}, user_params)
     Repo.insert(changeset)
     redirect(conn, to: Routes.user_path(conn, :index))
-
   end
 
+  def edit(conn, %{"id" => id}) do
+    user = Repo.get!(User, id)
+    changeset = User.changeset(user, %{})
+    render(conn, "edit.html", user: user, changeset: changeset)
+  end
+
+  def update(conn, %{"id" => id, "user" => user_params}) do
+    user = Repo.get!(User, id)
+    changeset = User.changeset(user, user_params)
+    Repo.update(changeset)
+    redirect(conn, to: Routes.user_path(conn, :index))
+  end
 end
